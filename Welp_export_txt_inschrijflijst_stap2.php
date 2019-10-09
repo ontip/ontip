ddf89ef687ed72cc54118b3b8c6093412f36d626
Erik Hendrikx <erik.hendrikx@gmail.com>
2019-10-09 12:28:42 +0200

    fopen ipbv header

--- Welp_export_txt_inschrijflijst_stap2.php
+++ Welp_export_txt_inschrijflijst_stap2.php
@@ -1,8 +1,33 @@
 <?php
+# 9okt2019          -            E. Hendrikx 
+# Symptom:   		    None.
+# Problem:     	    Aantal spelers niet goed opgehaald ivm verkeerd commando
+# Fix:              None
+# Feature:          None
+# Reference: 
+
+
+
 ob_start();
 $toernooi = $_GET['toernooi'];
-header("Content-type: application/octet-stream");
-header("Content-Disposition: attachment; filename=\"OnTip_".$toernooi.".txt\"");
+//header("Content-type: application/octet-stream");
+//header("Content-Disposition: attachment; filename=\"OnTip_".$toernooi.".txt\"");
+
+
+
+    $log = 'OnTip_'.$toernooi.'.txt';
+ //  echo $log;
+	
+	
+    if (file_exists($log)){
+    	unlink($log);
+    }
+  
+    $myfile = fopen($log, "w") or die("Unable to open file!");
+    $now = date('Y-m-d h:i:s');
+ 
+  
+
 
 
 // Database gegevens. 
@@ -20,10 +45,10 @@ else {
 		echo " Geen toernooi bekend :";
 };
 
-
-$aant_splrs_q = mysqli_query($con,"SELECT Count(*) from inschrijf WHERE Toernooi = '".$toernooi."' and Vereniging = '".$vereniging."' ")        or die('Fout in select 2'); 
-/// Bepalen aantal spelers voor dit toernooi
-$aant_splrs =  mysql_result($aant_splrs_q ,0); 
+$aant_splrs_q = mysqli_query($con,"SELECT Count(*) as Aantal  from inschrijf WHERE Toernooi = '".$toernooi."' and Vereniging = '".$vereniging."' ")        or die('Fout in select 2'); 
+//$aant_splrs =  mysql_result($aant_splrs_q ,0); 
+$result      = mysqli_fetch_array( $aant_splrs_q );
+$aant_splrs  = $result['Aantal'];
 
 // Inschrijven als individu of vast team
 
@@ -38,11 +63,13 @@ if  ($inschrijf_methode == ''){
 
 // header bestand
 
-echo "<OnTip>\r\n";
-echo "Equipes = ". $aant_splrs. "\r\n";
+//echo "<OnTip>\n";
+//echo "Equipes = ". $aant_splrs. "\n";
 
 
-
+  fwrite($myfile, "<OnTip>"); 
+  fwrite($myfile, "\nEquipes = ".$aant_splrs);    
+  
 
 switch($soort_inschrijving){
  	   case 'single'  : $soort = 1; break;
@@ -59,8 +86,8 @@ if ($inschrijf_methode == 'single'){
 }
 
 /// soort toernooi
-echo "Spelers = ". $soort . "\r\n";
-
+//echo "Spelers = ". $soort . "\n";
+fwrite($myfile, "\nSpelers = ".$soort);   
 
 //// SQL Queries
 $spelers      = mysqli_query($con,"SELECT * from inschrijf Where Toernooi = '".$toernooi."' and Vereniging = '".$vereniging."' order by Inschrijving" )    or die('Fout in select 3');  
@@ -69,81 +96,106 @@ $i=1;
 
 while($row = mysqli_fetch_array( $spelers )) {
 
-echo "Eq".$i. " = ";
+//echo "Eq".$i. " = ";
+fwrite($myfile, "\nEq".$i. " = " );   
+
 
   /// aanmaak details regels
 
 if ($soort_inschrijving =='single' or $inschrijf_methode == 'single'){
 	if (is_numeric($row['Licentie1'])) {
-		  echo  $row['Licentie1']. ",";
+		
+		fwrite($myfile, $row['Licentie1']. "," ); 
+//		  echo  $row['Licentie1']. ",";
 	 } else {
-	 	 echo   ",";
+	 		fwrite($myfile, ","); 
+	// 	 echo   ",";
 	}	  
-	  echo  $row['Naam1']. ";";
+	//  echo  $row['Naam1']. ";";
+	  	fwrite($myfile, $row['Naam1']. ";"); 
 }
   
  if ($soort_inschrijving !='single' and $inschrijf_methode != 'single'){
  	if (is_numeric($row['Licentie1'])) {
-		  echo  $row['Licentie1']. ",";
+ 			fwrite($myfile, $row['Licentie1']. "," ); 
+	//	  echo  $row['Licentie1']. ",";
 	 } else {
-	 	 echo   ",";
+	// 	 echo   ",";
+	   	fwrite($myfile,  ", "); 
 	}	  
-    echo  $row['Naam1']. ";";
+  //  echo  $row['Naam1']. ";";
+     	fwrite($myfile, $row['Naam1']. ";"); 
  }
  
 if ($inschrijf_methode != 'single' and ($soort_inschrijving == 'doublet'or  $soort_inschrijving == 'triplet' or $soort_inschrijving == 'kwintet' or $soort_inschrijving == 'sextet')){
     if (is_numeric($row['Licentie2'])) {
-		  echo  $row['Licentie2']. ",";
+		//  echo  $row['Licentie2']. ",";
+		  	fwrite($myfile, $row['Licentie2']. "," ); 
 	 } else {
-	 	 echo   ",";
+	// 	 echo   ",";
+	 	     	fwrite($myfile,  ","); 
 	}	  
-    echo  $row['Naam2']. ";";
+  //  echo  $row['Naam2']. ";";
+       	fwrite($myfile, $row['Naam2']. ";"); 
 }
 
  if ($inschrijf_methode != 'single' and ($soort_inschrijving == 'triplet' or $soort_inschrijving == 'kwintet' or $soort_inschrijving == 'sextet')){
     if (is_numeric($row['Licentie3'])) {
-		  echo  $row['Licentie3']. ",";
+		//  echo  $row['Licentie3']. ",";
+		  	fwrite($myfile, $row['Licentie3']. "," ); 
 	 } else {
-	 	 echo   ",";
+	// 	 echo   ",";
+	   	fwrite($myfile,  ","); 
 	}	  
-    echo  $row['Naam3']. ";";
+   // echo  $row['Naam3']. ";";
+       	fwrite($myfile, $row['Naam3']. ";"); 
   }
  
   if ($inschrijf_methode != 'single' and ($soort_inschrijving  == '4x4' or  $soort_inschrijving  == 'kwintet' or $soort_inschrijving == 'sextet')){
  
   	if (is_numeric($row['Licentie4'])) {
-		  echo  $row['Licentie4']. ",";
+		 // echo  $row['Licentie4']. ",";
+		  	fwrite($myfile, $row['Licentie4']. "," ); 
 	 } else {
-	 	 echo   ",";
+	// 	 echo   ",";
+	 	  	fwrite($myfile,  ","); 
 	}	  
-     echo  $row['Naam4']. ";";
+  //   echo  $row['Naam4']. ";";
+      	fwrite($myfile, $row['Naam4']. ";"); 
      
      if (is_numeric($row['Licentie5'])) {
-		  echo  $row['Licentie5']. ",";
+	//	  echo  $row['Licentie5']. ",";
+		  	fwrite($myfile, $row['Licentie5']. "," ); 
 	 } else {
-	 	 echo   ",";
+	 	// echo   ",";
+	 	     	fwrite($myfile,  ","); 
 	}	  
-     echo  $row['Naam5']. ";";
+  //   echo  $row['Naam5']. ";";
+        	fwrite($myfile, $row['Naam5']. ";"); 
   }
 
   if ($inschrijf_methode != 'single' and $soort_inschrijving  == 'sextet'){
      if (is_numeric($row['Licentie6'])) {
-	   	  echo  $row['Licentie6']. ",";
+	  // 	  echo  $row['Licentie6']. ",";
+	   	  	fwrite($myfile, $row['Licentie2']. "," ); 
 	     } else {
-	 	    echo   ",";
+	 	//    echo   ",";
+	 	    	fwrite($myfile,  ","); 
 	    }	  
-     echo  $row['Naam6']. ";";
+ //    echo  $row['Naam6']. ";";
+     	fwrite($myfile, $row['Naam6']. ";"); 
    }
-   
+   	  
 
-   
-echo "\r\n"; 
+ 
+//echo "\n"; 
 
 $i++;
 };
 
-
-
+ fclose($myfile);
+ 
 
 
 ?>
+Welp file: <a href = '<?php echo $log;?>' ><?php echo $log;?></a>
